83f1b3204d8ad750307d5afd69b90f320acba73c
seky739 <seky739@seznam.cz>
2017-09-26 13:17:38 +0200

    Nacitani napsaneho radku
    Nacteni napsaneho radku s tim ze akceptuji i presmerovani ze souboru na
    vstup. Bere jeho testovaci priklad

--- src/user/shell.cpp
+++ src/user/shell.cpp
@@ -1,33 +1,37 @@
 #include "shell.h"
 
 #include "rtl.h"
-
-#include <windows.h>
-
-
+#include<Windows.h>
+#include <iostream>
+#include <string>
+//using namespace std;
 
 using namespace std;
 
 size_t __stdcall shell(const kiv_os::TRegisters &regs) {
 	
-	kiv_os::THandle stdin = kiv_os_rtl::Create_File("CONOUT$", /*FILE_SHARE_WRITE*/2);	//nahradte systemovym resenim, zatim viz Console u CreateFile na MSDN
-	AllocConsole();
-	DWORD written ;
-	WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), "HESLO", strlen("HESLO"), &written, NULL);
+	//kiv_os::THandle stdin = kiv_os_rtl::Create_File("CONOUT$", /*FILE_SHARE_WRITE*/2);	//nahradte systemovym resenim, zatim viz Console u CreateFile na MSDN
 	
 	
-	const char* hello = "Hello world!\n";
+	//kiv_os::THandle stdin = kiv_os_rtl::Create_File("CONIN$ ", /*FILE_SHARE_WRITE*/2);
+	//AllocConsole();
+	//DWORD written ;
+	//WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), "HESLO", strlen("HESLO"), &written, NULL);
+	
+	
+	//const char* hello = "Hello world!\n";
 	//size_t written;
-	//string input = "open";
+	string input = "";
 	//kiv_os_rtl::Write_File(stdin, hello, /*strlen(hello)*/13, written);
 	//kiv_os_rtl::Close_File(stdin);
-	/*while (!input.empty()) {
+	while (true) {
 		//cout << "Please enter a valid sentence (with spaces):\n>";
-		cout << "Fuck@shell> ";
-		kiv_os_rtl::Write_File(stdin, hello, /*strlen(hello)13, written);
-		getline(stdin);
-		//cout << "You entered: " << input << endl << endl;
-	}*/
+		cout << "Own@shell> ";
+		//kiv_os_rtl::Write_File(stdin, hello, /*strlen(hello)13, written);
+		getline(cin,input);
+		cout << "You entered: " << input << endl << endl;
+		if (cin.eof())break;
+	}
 
 	
 	return 0;
